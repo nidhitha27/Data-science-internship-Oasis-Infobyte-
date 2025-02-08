7bd56489957e07665b0f015754556118a1c62a19
nidhitha27 <nidhitha27@gmail.com>
2025-02-08 17:39:49 +0530

    Delete label_encoder.py

--- label_encoder.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import joblib
-from sklearn.preprocessing import LabelEncoder
-clf = joblib.load('iris_classifier.pkl')
-
-label_encoder = LabelEncoder()
-label_encoder.classes_ = np.load('label_encoder.npy',allow_pickle=True)
-
